9466e109ea79b0d51cc8e715f0efbfb8cedc59b8
iancasyana16 <iancasyana@gmail.com>
2025-12-12 12:26:35 +0700

    update UserSeeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class UserSeeder extends Seeder
 {
@@ -12,6 +14,20 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $users = [
+            ['name' => 'Super Admin', 'email' => 'super@admin.com', 'password' => Hash::make('password'), 'role' => 'super admin'],
+            ['name' => 'Manager', 'email' => 'manager@org.com', 'password' => Hash::make('password'), 'role' => 'manager'],
+            ['name' => 'Staff', 'email' => 'staff@org.com', 'password' => Hash::make('password'), 'role' => 'staff'],
+            ['name' => 'Finance', 'email' => 'finance@org.com', 'password' => Hash::make('password'), 'role' => 'finance'],
+        ];
+
+        foreach ($users as $user) {
+            $user = User::create([
+                'name' => $user['name'],
+                'email' => $user['email'],
+                'password' => $user['password'],
+            ]);
+            $user->assignRole($user['role']);
+        }
     }
 }
